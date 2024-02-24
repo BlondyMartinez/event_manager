e93787ca462e93211ab6f345db3bdb4f90c2b3a7
Blondy <blondymartinezm@gmail.com>
2024-02-24 23:33:41 +0100

    reorganized

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -42,24 +42,40 @@ def save_thank_you_letter(id,form_letter)
     end
 end
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
-
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
-
-contents.each do |row|
-    id = row[0]
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    phone = clean_phone(row[:homephone])
-    # legislators = legislators_by_zipcode(zipcode)
-  
-    puts name + " " + phone
-    # form_letter = erb_template.result(binding)
-  
-    # save_thank_you_letter(id,form_letter)
+def get_contents
+    contents = CSV.open(
+    'event_attendees.csv',
+    headers: true,
+    header_converters: :symbol
+    )
 end
+
+def create_letters
+    template_letter = File.read('form_letter.erb')
+    erb_template = ERB.new template_letter
+    
+    contents = get_contents
+
+    contents.each do |row|
+        id = row[0]
+        name = row[:first_name]
+        zipcode = clean_zipcode(row[:zipcode])
+        legislators = legislators_by_zipcode(zipcode)
+    
+        form_letter = erb_template.result(binding)
+    
+        save_thank_you_letter(id, form_letter)
+    end
+end
+
+def get_phone_numbers
+    contents = get_contents
+
+    contents.each do |row|
+        phone = clean_phone(row[:homephone])
+        puts phone
+    end
+end
+
+# create_letters
+# get_phone_numbers
